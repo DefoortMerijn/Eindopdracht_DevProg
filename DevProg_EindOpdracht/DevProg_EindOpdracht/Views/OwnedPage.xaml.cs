64e7862eecee89b48f00ab7f518c3d1684a6da87
Merijn Defoort <merijn.defoort@student.howest.be>
2021-12-08 15:48:16 +0100

    Added: OwnedPage

---
+++ DevProg_EindOpdracht/DevProg_EindOpdracht/Views/OwnedPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DevProg_EindOpdracht.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class OwnedPage : ContentPage
+    {
+        public OwnedPage()
+        {
+            InitializeComponent();
+        }
+
+
+        private void TapGestureOverview_Tapped(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new MainPage());
+
+            Debug.WriteLine("OverwiewPage");
+        }
+
+        private void TapGestureOwned_Tapped(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new OwnedPage());
+
+            Debug.WriteLine("OwnedPage");
+        }
+    }
+}
